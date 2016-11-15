0d2b3804078ca955af668515f96bde1e7f70b149
john <wildfireone@gmail.com>
2016-11-15 22:02:17 +0000

    changing accelerometer to angle orientation

--- js/multicontrol.js
+++ js/multicontrol.js
@@ -4,12 +4,12 @@ var goingup;
 var player1move = firebase.database().ref('moves/player1/y');
 player1move.on('value', function(snapshot) {
   if(game){
-    console.log(snapshot.val())
+    //console.log(snapshot.val())
     var angle = Math.tan(snapshot.val());
     var adjacent = (canvas.height/2);
     var opposite = adjacent * angle;
     y = opposite;
-    console.log(y);  
+    document.getElementById("angle").innerHTML = snapshot.val() + ":"+y
     updatePlayer(1,y);
   }
 });
