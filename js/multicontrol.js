4bc399545b7280a3e544807e4f9c78e173938fe9
john <wildfireone@gmail.com>
2016-11-15 20:06:15 +0000

    debugging fiebase move player

--- js/multicontrol.js
+++ js/multicontrol.js
@@ -3,6 +3,14 @@
 
 var player1move = firebase.database().ref('moves/player1/y');
 player1move.on('value', function(snapshot) {
-console.log(snapshot.val())
-  //updatePlayer(1, snapshot.val().y);
+  console.log(snapshot.val())
+  var y = 0;
+  if(y >0){
+    y = game.player.y + 1;
+  }
+  else if(y<0){
+    y = game.player.y -1;
+  }
+
+  updatePlayer(1,y);
 });
