fbbd3f712a6bb6e569593f88e1af8735d82e4e5d
john <wildfireone@gmail.com>
2016-11-15 23:26:28 +0000

    changing accelerometer

--- js/multicontrol.js
+++ js/multicontrol.js
@@ -1,15 +1,15 @@
 
 
 var goingup;
-var player1move = firebase.database().ref('moves/player1/y');
+var player1move = firebase.database().ref('moves/player1/a');
 player1move.on('value', function(snapshot) {
   if(game){
     //console.log(snapshot.val())
     var y = snapshot.val();
 
-    y = game.player.y - (2*y);//(2+(2*y));
-    if(y>canvas.height){y = canvas.height;}
-    if(y<0){y=0;}
+    y = game.player.y + (y);//(2+(2*y));
+    //if(y>canvas.height){y = canvas.height;}
+    //if(y<0){y=0;}
 
     updatePlayer(1,y);
     document.getElementById('angle').innerHTML = ""+snapshot.val()+"      :    "+y;
