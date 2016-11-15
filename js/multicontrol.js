54157e3942dd9ee7d4d4a008277a3c04fee25867
john <wildfireone@gmail.com>
2016-11-15 20:41:34 +0000

    fixing player issues

--- js/multicontrol.js
+++ js/multicontrol.js
@@ -3,14 +3,16 @@
 
 var player1move = firebase.database().ref('moves/player1/y');
 player1move.on('value', function(snapshot) {
-  console.log(snapshot.val())
-  var y = snapshot.val();
-  if(y >0){
-    y = game.player.y + 1;
-  }
-  else if(y<0){
-    y = game.player.y -1;
-  }
+  if(game){
+    console.log(snapshot.val())
+    var y = snapshot.val();
+    if(y >0){
+      y = game.player.y + 1;
+    }
+    else if(y<0){
+      y = game.player.y -1;
+    }
 
-  updatePlayer(1,y);
+    updatePlayer(1,y);
+  }
 });
