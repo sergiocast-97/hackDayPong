362fec4fe0c6d5230082326c960d49954943a363
john <wildfireone@gmail.com>
2016-11-15 23:05:45 +0000

    changing accelerometer

--- js/multicontrol.js
+++ js/multicontrol.js
@@ -6,13 +6,13 @@ player1move.on('value', function(snapshot) {
   if(game){
     //console.log(snapshot.val())
     var y = snapshot.val();
-    document.getElementById('angle').innerHTML = ""+snapshot.val()+"      :    "+y;
+
     y = game.player.y + (2+(2*y));
     if(y>canvas.height){y = canvas.height;}
     if(y<0){y=0;}
 
     updatePlayer(1,y);
-
+    document.getElementById('angle').innerHTML = ""+snapshot.val()+"      :    "+y;
   }
 });
 
