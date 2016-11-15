af04f63da5876caa75aacd9e84665373cb2dd4a4
john <wildfireone@gmail.com>
2016-11-15 19:46:14 +0000

    added y to multicontrol

--- js/multicontrol.js
+++ js/multicontrol.js
@@ -3,5 +3,5 @@
 
 var player1move = firebase.database().ref('moves/player1/y');
 player1move.on('value', function(snapshot) {
-  updatePlayer(1, snapshot.val());
+  updatePlayer(1, snapshot.val().y);
 });
