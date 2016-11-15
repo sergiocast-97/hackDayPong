aea2fb33b7b2b4082e15b7a4c351e0fc69d22e8d
john <wildfireone@gmail.com>
2016-11-15 23:39:24 +0000

    changing accelerometer

--- js/multicontrol.js
+++ js/multicontrol.js
@@ -5,7 +5,7 @@ var player1move = firebase.database().ref('moves/player1/a');
 player1move.on('value', function(snapshot) {
   if(game){
     //console.log(snapshot.val())
-    var y = (canvas.height/6) * snapshot.val();
+    var y = (canvas.height/12) * snapshot.val();
 
     y = game.player.y + (y);//(2+(2*y));
     if(y>canvas.height){y = canvas.height;}
